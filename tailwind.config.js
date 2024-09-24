93119d4a1a0476ebe85dff76fcfa4adbf3e2b999
esmanurertay <esmanurertayy@gmail.com>
2024-09-24 03:28:44 +0300

    Tailwind Movie Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true, 
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      }
+    },
+
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+      colors: {
+        'ene-red':'red',
+        'ene-yellow': 'yellow',
+        'ene-grey' : 'grey',
+        'ene-white': 'white',
+      },
+    },
   },
   plugins: [],
 }
